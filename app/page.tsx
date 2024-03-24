111e2f074bc4c6c17e546c47a912010b1ebb8621
ComboRush <comborush.init@proton.me>
2024-03-24 22:33:19 +0200

    for making animation later

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
 "use client"
 
 import { Text } from "react-font";
+import { motion } from "framer-motion"
 export default function Home() {
   return (
+    <motion.section className="w-[70rem] h-[90%]">
       </div>
       </div>
+    </motion.section>
   );
 }
