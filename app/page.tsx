e348fb7cdc23501712dce26203de06a6a7cc00c7
ComboRush <comborush.init@proton.me>
2024-03-24 22:34:44 +0200

    adding the containung divs

--- app/page.tsx
+++ app/page.tsx
@@ -8,6 +8,10 @@ export default function Home() {
       <div className="flex w-full h-fit">
         <Text family='Lexend' className="text-[8rem] m-auto font-extrabold text-gray-300 tracking-widest">SOFA</Text >
       </div>
+      <div className="bg-white w-full h-full rounded-t-3xl shadow-2xl shadow-black/50 p-5">
+        <div className="rounded-2xl bg-[#f7f7f7] w-full h-full p-5">
+          </Canvas>
+        </div>
       </div>
     </motion.section>
   );
