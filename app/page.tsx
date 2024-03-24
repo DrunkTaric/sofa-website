49521f61b474feac0dd7750e359352b79024fc17
ComboRush <comborush.init@proton.me>
2024-03-24 22:33:35 +0200

    adding the big text

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,8 @@ import { motion } from "framer-motion"
 export default function Home() {
   return (
     <motion.section className="w-[70rem] h-[90%]">
+      <div className="flex w-full h-fit">
+        <Text family='Lexend' className="text-[8rem] m-auto font-extrabold text-gray-300 tracking-widest">SOFA</Text >
       </div>
       </div>
     </motion.section>
